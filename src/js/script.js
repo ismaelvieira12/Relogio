1d7079d1dcaa4e17b8d9585b8fbcde1d2337d86f
Ismael Nasciemnto <ismaelvieira12@outlook.com>
2024-09-22 14:34:20 -0300

    Add script para o projeto

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,15 @@
 const relogio = document.querySelector('#relogio');
 const cronometro = document.querySelector("#cronometro");
 const boxTime = document.querySelector('.box-time');
+const timeCro = document.querySelector('.time-cro');
 
 
 relogio.addEventListener('click', () => {
-    // boxTime.classList.add('hidden');
+    boxTime.classList.add('show');
     console.log('Teste');
-}, {once:true});
+});
+
+cronometro.addEventListener('click', () => {
+    timeCro.classList.add('show');
+    boxTime.classList.add('hidden');
+})
