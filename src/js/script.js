406e1ae0420181b2bc7aecd4e691b484f47208e2
Ismael Nasciemnto <ismaelvieira12@outlook.com>
2024-09-22 14:12:11 -0300

    Estilizando a parte do cronometro

---
+++ src/js/script.js
@@ -0,0 +1,9 @@
+const relogio = document.querySelector('#relogio');
+const cronometro = document.querySelector("#cronometro");
+const boxTime = document.querySelector('.box-time');
+
+
+relogio.addEventListener('click', () => {
+    // boxTime.classList.add('hidden');
+    console.log('Teste');
+}, {once:true});
