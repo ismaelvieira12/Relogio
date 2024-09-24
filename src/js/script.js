e98b6f5d8de2069bc97f320e5b469c1514ae829b
Ismael Nasciemnto <ismaelvieira12@outlook.com>
2024-09-24 13:36:03 -0300

    reestruturando o código.

--- src/js/script.js
+++ src/js/script.js
@@ -1,15 +1,41 @@
-const relogio = document.querySelector('#relogio');
-const cronometro = document.querySelector("#cronometro");
-const boxTime = document.querySelector('.box-time');
-const timeCro = document.querySelector('.time-cro');
+// const relogio = document.querySelector('#relogio');
+// const cronometro = document.querySelector("#cronometro");
+// const boxTime = document.querySelector('.box-time');
+// const timeCro = document.querySelector('.time-cro');
 
 
-relogio.addEventListener('click', () => {
-   boxTime.classList.add('show');
-   timeCro.style.display="none";
-});
+// relogio.addEventListener('click', () => {
+//    boxTime.classList.add('show');
+//    timeCro.style.display="none";
+// });
 
-cronometro.addEventListener('click', () => {
-    timeCro.style.display='flex'
-    boxTime.style.display='none';
+// cronometro.addEventListener('click', () => {
+//     timeCro.style.display='flex'
+//     boxTime.style.display='none';
+// })
+
+const hora = document.querySelector('.hora');
+const minuto = document.querySelector('.minuto');
+const segundo = document.querySelector('.segundo');
+
+const relogio = setInterval( () =>{
+    let timeToday = new Date();
+
+    let h = timeToday.getHours();
+    let m = timeToday.getMinutes();
+    let s = timeToday.getSeconds();
+
+    if(h < 10){
+        h = `0 ${h}`;
+    }
+    else if(m < 10){
+        m = `0 ${m}`
+    }
+    else if(s < 10){
+        s = `0${s}`
+    }
+
+    hora.textContent = h;
+    minuto.textContent = m;
+    segundo.textContent = s; 
 })
