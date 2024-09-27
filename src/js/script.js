a68763df0b609bf721ae4affcb7c7a9ffced002a
Ismael Nasciemnto <ismaelvieira12@outlook.com>
2024-09-26 23:41:45 -0300

    update

--- src/js/script.js
+++ src/js/script.js
@@ -18,24 +18,47 @@ const hora = document.querySelector('.hora');
 const minuto = document.querySelector('.minuto');
 const segundo = document.querySelector('.segundo');
 
-const relogio = setInterval( () =>{
-    let timeToday = new Date();
+// const relogio = setInterval( () =>{
+//     let timeToday = new Date();
 
-    let h = timeToday.getHours();
-    let m = timeToday.getMinutes();
-    let s = timeToday.getSeconds();
+//     let h = timeToday.getHours();
+//     let m = timeToday.getMinutes();
+//     let s = timeToday.getSeconds();
+
+//     if(h < 10){
+//         h = `0 ${h}`;
+//     }
+//     else if(m < 10){
+//         m = `0 ${m}`
+//     }
+//     else if(s < 10){
+//         s = `0${s}`
+//     }
+
+//     hora.textContent = h;
+//     minuto.textContent = m;
+//     segundo.textContent = s; 
+// }
+
+
+const relogio = setInterval(() =>{
+    let time = new Date();
+
+    let h = time.getHours();
+    let m = time.getMinutes();
+    let s = time.getSeconds();
 
     if(h < 10){
-        h = `0 ${h}`;
+        h = `0${h}`;
     }
     else if(m < 10){
-        m = `0 ${m}`
+        m = `0${m}`;
     }
     else if(s < 10){
-        s = `0${s}`
+        s = `0${s}`;
     }
 
-    hora.textContent = h;
-    minuto.textContent = m;
-    segundo.textContent = s; 
-})
+    hora.textContent= h;
+    minuto.textContent= m;
+    segundo.textContent= s;
+});
