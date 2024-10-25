09dd7ed43c35788082480f884a0b6cfe8a2d9060
Ismael Nasciemnto <ismaelvieira12@outlook.com>
2024-10-25 11:50:09 -0300

    fazendo os primeiros testes das function

--- src/js/crono.js
+++ src/js/crono.js
@@ -7,3 +7,46 @@ const minutes = document.querySelector('.minutes');
 const seconds = document.querySelector('.seconds')
 
 //Criando a function que irá startar o cronometro
+const atualizarDisplay = () =>{
+    const h = String(Math.floor(tempo / 3600)).padStart(2, '0');
+    const m = String(Math.floor((tempo % 3600) / 60)).padStart(2, '0');
+    const s = String(Math.floor(tempo % 60)).padStart(2, '0');
+
+    hours = document.textContent = h;
+    minutes = document.textContent = m;
+    seconds = document.textContent = s;
+}
+
+//O cronometro será iniciado mudando assim o valor da variavel
+// a variavel RODANDO será alterada para verdadeira e será atribuida uma fucttion para a 
+// a variavel intervalo que acrescentará valor ao tempo e por sua vez chama a function atualizar display
+const iniciarCronometro = () => {
+    if(!rodando){
+        rodando = true;
+        intervalo = setInterval = (() =>{
+            tempo++
+            atualizarDisplay();
+        }, 1000)
+    }
+}
+
+//para esssa fuction so precisamos voltar a variavel RODANDO para o valor inicial
+// e depois para a fuction que foi a tribuida para a varial INTERVALO
+const pausarCronometro = () => {
+    rodando = false;
+
+    clearInterval(intervalo);
+}
+
+
+// para voltar e restartar o cronometro basta chamar as duas function 
+// pausarCronometro e iniciar o cronometro e voltar o tempo para 0
+const restartar = () =>{
+    pausarCronometro();
+    tempo = 0;
+    iniciarCronometro();
+}
+
+
+// iniciarCronometro();
+// atualizarDisplay();
