5cfd1bc2d3d7fe1389aef998297da1019ace1cbe
Ismael Nasciemnto <ismaelvieira12@outlook.com>
2024-10-26 13:30:35 -0300

    update

--- src/js/relogio.js
+++ src/js/relogio.js
@@ -29,12 +29,15 @@ const relogio = setInterval(() =>{
     let s = time.getSeconds();
 
     if(h < 10){
-        h = `0${h}`
-    }if(m < 10) {
-        m = `0${m}`
-    }if(s < 10) {
+        h = `0 ${h}`;
+    }
+    else if(m < 10){
+        m = `0 ${m}`
+    }
+    else if(s < 10){
         s = `0${s}`
     }
+
     
     hora.textContent = h;
     minuto.textContent = m;
