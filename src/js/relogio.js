407b7266fe5f066d74c263858bb63dce433893f5
Ismael Nasciemnto <ismaelvieira12@outlook.com>
2024-10-28 07:56:59 -0300

    Estilizado o placeMark

--- src/js/relogio.js
+++ src/js/relogio.js
@@ -30,16 +30,15 @@ const relogio = setInterval(() =>{
 
     if(h < 10){
         h = `0${h}`;
-    }
-    else if(m < 10){
-        m = `0 ${m}`
-    }
-    else if(s < 10){
+    }if(m < 10){
+        m = `0${m}`
+    }if(s < 10){
         s = `0${s}`;
     }
 
-    
     hora.textContent = h;
     minuto.textContent = m;
     segundo.textContent = s; 
-})
+});
+
+
