36ff8986b54902c47b72bf254fb010f330c67ce7
Ismael Nasciemnto <ismaelvieira12@outlook.com>
2024-10-30 09:00:12 -0300

    Finalizando a função de resetar o tempo

--- src/js/teste.js
+++ src/js/teste.js
@@ -19,9 +19,18 @@ let minutes = 0;
 let seconds = 0;
 let miliSeconds = 0;
 
+function updateDisplay() {
+    document.getElementById("timer").textContent =
+        (hours < 10 ? "0" + hours : hours) + ":" +
+        (minutes < 10 ? "0" + minutes : minutes) + ":" +
+        (seconds < 10 ? "0" + seconds : seconds) + ":" +
+        (miliSeconds < 10 ? "0" + miliSeconds : miliSeconds);   
+}
 
 //Função para iniciar o cronometro
 const startTimerStopGame = () => {
+    //Para que o cronometro seja pausado a variavel (rodando) tem que ser verdadeira
+    // logo após setamos ela como (false) para que o cronometro possa ser reiniciado novamente
     if(rodando){
         clearInterval(intervalo);
         rodando = false;
@@ -54,21 +63,13 @@ const startTimerStopGame = () => {
             sEl.textContent = formatTime(seconds);
             milsEl.textContent = formatTimeMiliSeconds(miliSeconds);
             rodando = true;
+            updateDisplay();
         
         }, 10);
     
         play.innerHTML=innerHTML= '<i class="fa-solid fa-pause"></i>';
     }
 }
-
-// Fuction para pausar o cronometro
-const puseTimer = () => {
-    rodando = true;
-    clearInterval(intervalo);
-
-    play.innerHTML= '<i class="fa-solid fa-play"></i>'
-}
-
 
 // criando uma função para formatação dos numeros do cronometro colocando o ZERo na frente
 const formatTime = (numbers) => {
@@ -79,7 +80,20 @@ const formatTimeMiliSeconds = (numbers) => {
     return numbers < 100 ? `${numbers}`.padStart(3, '0') : numbers;
 }
 
+
+// função para resetar o tempo
+const restartarTime = () => {
+    play.innerHTML= '<i class="fa-solid fa-play"></i>';
+
+    clearInterval(intervalo);
+    rodando = false;
+    hours = 0;
+    minutes = 0;
+    seconds = 0;
+    miliSeconds = 0;
+    updateDisplay();
+
+}
+
 play.addEventListener('click', startTimerStopGame);
-// pouse.addEventListener('click', puseTimer);
-
-
+refresh.addEventListener('click', restartarTime);
