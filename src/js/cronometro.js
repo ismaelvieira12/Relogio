648f04875c29add6baae44886e0ef05d42c98c83
Ismael Nasciemnto <ismaelvieira12@outlook.com>
2024-11-03 12:45:29 -0300

    Feito update

--- src/js/cronometro.js
+++ src/js/cronometro.js
@@ -98,7 +98,7 @@ const restartarTime = () => {
 const salvarRecorde = () => {
     const recordList = document.querySelector('#record-list');
     const novoRecord =  document.createElement('li');
-    const trashRecord = document.createElement('button');
+    const trashRecord = document.createElement('a');
 
     
     // Formatando o tempo para que seja salvo na lista de recordes
@@ -108,16 +108,20 @@ const salvarRecorde = () => {
     
     
     //estilizando o novoRecord CSS
-    novoRecord.style.display='flex';
-    novoRecord.style.justifyContent='space-between';
-    novoRecord.style.alignItems='center';
-    trashRecord.style.display='flex';
-    trashRecord.style.justifyContent='center';
-    trashRecord.style.alignItems='center';
-    trashRecord.style.justifyItems='center';
-    trashRecord.style.height='100%';
+    recordList.style.display='flex';
+    recordList.style.flexDirection='column';
+    recordList.style.justifyContent='space-between';
+    recordList.style.alignItems='center';
+    novoRecord.style.gap='30px';
+    novoRecord.style.width='100%';
+    // trashRecord.style.display='flex';
+    // trashRecord.style.justifyContent='center';
+    // trashRecord.style.alignItems='center';
+    // trashRecord.style.justifyItems='center';
+    // trashRecord.style.height='100%';
     trashRecord.style.fontSize='20px';
-    trashRecord.style.color='#B88B4A'
+    trashRecord.style.color='#B88B4A';
+    // trashRecord.style.marginBottom='10px';
 
     
     trashRecord.innerHTML='<i class="fa-solid fa-trash"></i>';
