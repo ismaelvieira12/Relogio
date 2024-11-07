e79eaafb2ec69efaea06a150b8c32159b8bbeceb
Ismael Nasciemnto <ismaelvieira12@outlook.com>
2024-11-06 23:07:57 -0300

    update

--- src/js/relogio.js
+++ src/js/relogio.js
@@ -51,9 +51,10 @@ const relogio = setInterval(() =>{
 
 
 const addAlarme = () => {
-    const option = document.createElement('option');
-    // option.innerHTML='horas'
-    alarmaList.appendChild(option);
+
+    const select = document.createElement('select');
+    select.innerHTML='<option>Horas</option> <option>01</option> <option>02</option> <option>03</option> <option>04</option> <option>05</option> <option>06</option> <option>07</option> <option>08</option> <option>09</option><option>10</option> <option>11</option> <option>12</option> ';
+    alarmaList.appendChild(select);
 }
 
 btnAlarme.addEventListener('click', addAlarme);
